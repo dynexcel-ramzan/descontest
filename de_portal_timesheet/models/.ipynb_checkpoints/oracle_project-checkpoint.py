5102673820e3f8de0c7137fc4347b4f16f04c646
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-11-17 13:23:25 +0000

    update portal timesheet

--- de_portal_timesheet/models/.ipynb_checkpoints/oracle_project-checkpoint.py
+++ de_portal_timesheet/models/.ipynb_checkpoints/oracle_project-checkpoint.py
@@ -9,11 +9,22 @@ from odoo.exceptions import UserError, ValidationError
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
+    ora_project_id = fields.Integer(string='ORA Project id')
+
     def action_fetch_oracle_project(self):
-        conn = cx_Oracle.connect('xx_odoo/xxodoo123$@//10.8.7.152:1523/test2')
+        conn = cx_Oracle.connect('xx_odoo/xxodoo123$@//192.168.65.152:1523/test2')
         cur = conn.cursor()
-        statement = "select * from XX_APPS_IPL_PROJECTS"
+        statement = "select * from XX_APPS_IPL_PROJECTS where status='APPROVED' and enabled='Y'"
         cur.execute(statement)
         projects = cur.fetchall()
-        raise UserError(str(projects))
-
+        # raise UserError(str(projects))
+        for prj in projects:
+            prj_value = {
+                 'ora_project_id': prj[0],
+                'name': prj[1]+' ('+str(prj[2])+') ',
+            }
+            project = self.env['project.project'].sudo().create(prj_value)
+            client_value = {
+                'name': prj[4] + ' (' + str(prj[3]) + ') ',
+            }
+            client = self.env['res.ora.client'].sudo().create(client_value)
