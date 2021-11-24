26439a0efe2c5847a715130fb0068dc248a5687c
gitahsan <ahsan.s@dynexcel.co>
2021-11-24 17:48:51 +0000

    update promotion

--- de_hr_employee_promotion/models/employee_promotion.py
+++ de_hr_employee_promotion/models/employee_promotion.py
@@ -15,18 +15,41 @@ class EmployeePromotion(models.Model):
     previous_wage = fields.Monetary(string='Previous Wage')
     pre_job_id = fields.Many2one('hr.job', string="Previous ")
     description = fields.Char(string="Description")
-    date = fields.Date(string="Date")
+    date = fields.Date(string="Date", required=True)
     promotion_type = fields.Char(string="Promotion Type")
-    new_wage = fields.Char(string="New Salary")
+    new_wage = fields.Monetary(string="New Salary")
     new_job_id = fields.Many2one('hr.job', string="New Designation")
-    
-    
+    period = fields.Selection( 
+        [
+        ('01', 'Jan'),
+        ('02', 'Feb'),
+        ('03', 'March'),
+        ('04', 'April'),
+        ('05', 'May'),
+        ('06', 'Jun'),
+        ('07', 'July'),
+        ('08', 'Aug'),
+        ('09', 'Sep'),
+        ('10', 'Oct'),
+        ('11', 'Nov'),
+        ('12', 'Dec'),    
+         ],
+         string='Period')
+    tax_year = fields.Char(string='Tax Year')    
     old_department_id = fields.Many2one('hr.department', string='Old Department')
-    new_department_id = fields.Many2one('hr.department', string='New Department')
-    
+    new_department_id = fields.Many2one('hr.department', string='New Department')    
     loc_show_hide = fields.Boolean(string = "is Transfer?")
     old_location_id = fields.Many2one('hr.work.location', string='Old Work Location')
     new_location_id = fields.Many2one('hr.work.location', string='New Work Location')
+    
+    @api.onchange('date')
+    def onchange_date(self):
+        for line in self:
+            if line.date:
+                line.update({
+                    'tax_year': line.date.strftime('%Y'),
+                    'period': line.date.strftime('%m'),
+                })
     
     @api.onchange('employee_id')
     def onchange_employee(self):
@@ -52,6 +75,10 @@ class EmployeePromotion(models.Model):
     
     def action_update_contract_wage(self):
         for line in self:
+            line.update({
+                    'tax_year': line.date.strftime('%Y'),
+                    'period': line.date.strftime('%m'),
+                })
             line.contract_id.wage=line.new_wage
             if line.new_department_id:
                 line.employee_id.update({
